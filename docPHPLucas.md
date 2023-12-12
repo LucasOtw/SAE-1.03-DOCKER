449f061e4cc5727c281ff6c9d8ab47c20d947a24
Lucas Torres <lucastorres@lucass-mbp.home>
2023-12-12 19:15:17 +0100

    Poursuite du php et ajout d'une documentation

---
+++ docPHPLucas.md
@@ -0,0 +1,14 @@
+## Documentation de mon code PHP
+
+
+```file_put_contents()```
+
+#### Utilisée pour écrire dans un fichier
+
+```$htmlFile```
+
+### Nom du fichier dans lequel on enregistre
+
+```$htmlContent```
+
+### Chaine qui contient le code HTML
--- gendoc-user.php
+++ gendoc-user.php
@@ -1,3 +1,14 @@
-<?php
-
+<?php // Création du fichier html de manière dynamique
+$htmlContent = "<html> 
+<head>
+    <title>SAE1.03-Docker</title></head>
+    <body>
+        <h1>Test pour la conversion de fichier html => PDF</h1>
+            <p>CE texte est un exemple pour le test</p>
+    </body>
+</html>";
+$htmlFile = 'doc-tech-<0.1>.html'; // Enregistrement et renomage du fichier
+file_put_contents($htmlFile, $htmlContent); // Enregistrement du contenu de htmlContent dans une variable (htmlFile)
 ?>
+
+
