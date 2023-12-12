ef05c3c3c35d8c0ffda7dcc53163b16b69274356
Lucas Torres <lucastorres@lucass-mbp.home>
2023-12-12 19:26:02 +0100

    Poursuite du php

--- gendoc-user.php
+++ gendoc-user.php
@@ -9,6 +9,19 @@ $htmlContent = "<html>
 </html>";
 $htmlFile = 'doc-tech-<0.1>.html'; // Enregistrement et renomage du fichier
 file_put_contents($htmlFile, $htmlContent); // Enregistrement du contenu de htmlContent dans une variable (htmlFile)
+
+//Ajout d'une commande pour effectuer la conversion dans l'image docker ?
+
+$commandeDocker = 'docker run -v  -ti :/data image_docker_convert_html_to_pdf input.html output.pdf'; // Commande à effectuer dans le terminal
+$output = shell_exec($commandDocker); // Cette ligne effectue la commande dans php
+
+//Boucle de vérification de conversion 
+
+if ( $output === nul){ 
+    echo 'La conversion est terminée';
+} else {
+    echo 'La conversion a echouée';
+}
 ?>
 
 
